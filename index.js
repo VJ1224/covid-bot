8b3dfec3e727e1862ea8b8b350bb66c031ae5026
Vansh <vanshjain1224@gmail.com>
2020-06-21 22:06:58 +0530

    heroku

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
 const fs = require('fs');
 client.commands = new Discord.Collection();
 
