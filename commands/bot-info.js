846181880adf2ca78ae62833918d01b961881c6c
Vansh <vanshjain1224@gmail.com>
2020-06-26 22:11:38 +0530

    Updated bot info

--- commands/bot-info.js
+++ commands/bot-info.js
@@ -8,11 +8,11 @@ module.exports = {
 	execute(message, args) {
 		const infoEmbed = new Discord.MessageEmbed()
 		.setTitle('COVID-19 India Bot')
-		.setAuthor('Vansh Jain', '', 'https://github.com/VJ1224')
 		.setDescription('This is a COVID-19 Information bot that gives information regarding the cases in India.\n\n' +
 		'Use ' + process.env.PREFIX + 'help for a list of commands.'
 		)
-		.setTimestamp()
+		.setFooter('Author: Vansh Jain')
+		.setURL('https://github.com/VJ1224')
 
 		message.channel.send(infoEmbed)
 	},
