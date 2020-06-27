82fe05fc4b406ff3579b88d8677e7ad894439d8c
Vansh <vanshjain1224@gmail.com>
2020-06-27 13:16:17 +0530

    Removed intl dependency

--- tools.js
+++ tools.js
@@ -1,8 +1,10 @@
-global.Intl = require('intl');
-
 module.exports = function() {
-  this.toIndianFormat = function(string) {
-    number = parseInt(string);
-    return new Intl.NumberFormat('en-IN').format(number);
+  this.toIndianFormat = function(number) {
+    var lastThree = number.substring(number.length-3);
+    var otherNumbers = number.substring(0,number.length-3);
+    if(otherNumbers != '')
+        lastThree = ',' + lastThree;
+    var res = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
+    return res;
   }
 };
