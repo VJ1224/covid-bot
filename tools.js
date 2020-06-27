e7d0d79c2e1254fb794a8ce406bf63ad03041425
Vansh <vanshjain1224@gmail.com>
2020-06-27 13:01:42 +0530

    Switched to Indian number format

--- tools.js
+++ tools.js
@@ -1,5 +1,8 @@
+global.Intl = require('intl');
+
 module.exports = function() {
   this.toIndianFormat = function(string) {
-    return parseInt(string).toLocaleString('en-IN');
+    number = parseInt(string);
+    return new Intl.NumberFormat('en-IN').format(number);
   }
 };
