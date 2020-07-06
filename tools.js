d2946f52200673f55f030c7e0d68a64d2339d03a
Vansh Jain <vanshjain1224@gmail.com>
2020-07-06 18:35:06 +0530

    Formatting fix

--- tools.js
+++ tools.js
@@ -1,13 +1,14 @@
 const toIndianFormat = (number) => {
-    number = number.toString();
-    var lastThree = number.substring(number.length-3);
-    var otherNumbers = number.substring(0,number.length-3);
-    if(otherNumbers != '')
-        lastThree = ',' + lastThree;
-      var res = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
-    return res;
-}
+	number = number.toString();
+	let lastThree = number.substring(number.length - 3);
+	const otherNumbers = number.substring(0, number.length - 3);
+	if (otherNumbers != '') {
+		lastThree = ',' + lastThree;
+	}
+	const res = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ',') + lastThree;
+	return res;
+};
 
 module.exports = {
-  toIndianFormat
-}
+	toIndianFormat,
+};
