a3d7614196a8a2192e9bbb1b41def9f0d26ad0be
VJ1224 <vanshjain1224@gmail.com>
2020-08-20 17:20:44 +0530

    Added argument for number of states

--- commands/states.js
+++ commands/states.js
@@ -1,12 +1,11 @@
 const fetch = require('node-fetch');
-const Discord = require('discord.js');
 const { toIndianFormat } = require('../tools.js');
 
 module.exports = {
     name: 'states',
     description: 'India\'s COVID-19 cases sorted by state.',
     usage: ' ',
-    execute: async function (message) {
+    execute: async function (message, args) {
         const nationalData = await fetch('https://api.covid19india.org/data.json')
             .then(response => response.json())
             .catch(error => console.error(error));
@@ -18,34 +17,34 @@ module.exports = {
 
         statesArray.sort(function(a,b){return b['confirmed'] - a['confirmed']});
 
-        let description = '```';
-        description += addWhiteSpace('State', 15);
-        description += '| ' + addWhiteSpace('Confirmed', 10);
-        description += '| ' + addWhiteSpace('Active', 10);
-        description += '| ' + addWhiteSpace('Recovered', 10);
-        description += '| ' + addWhiteSpace('Deaths', 8);
+        let statesMessage = '**COVID-19 Cases by State**\n```';
+        statesMessage += addWhiteSpace('State', 20);
+        statesMessage += '| ' + addWhiteSpace('Confirmed', 10);
+        statesMessage += '| ' + addWhiteSpace('Active', 10);
+        statesMessage += '| ' + addWhiteSpace('Recovered', 10);
+        statesMessage += '| ' + addWhiteSpace('Deaths', 8);
 
-        let count = -1, limit = 10;
+        let count = -1, limit = (args.length >= 1) ? parseInt(args[0]) : 10;
 
         for (let state of statesArray) {
             count++;
             if (count === 0) continue;
-            if (count === limit) break;
+            if (count > limit) break;
 
-            description += `\n${addWhiteSpace(state['state'], 15)}`;
-            description += `| ${addWhiteSpace(toIndianFormat(state['confirmed']), 10)}`;
-            description += `| ${addWhiteSpace(toIndianFormat(state['active']), 10)}`;
-            description += `| ${addWhiteSpace(toIndianFormat(state['recovered']), 10)}`;
-            description += `| ${addWhiteSpace(toIndianFormat(state['deaths']), 8)}`;
+            statesMessage += `\n${addWhiteSpace(state['state'], 20)}`;
+            statesMessage += `| ${addWhiteSpace(toIndianFormat(state['confirmed']), 10)}`;
+            statesMessage += `| ${addWhiteSpace(toIndianFormat(state['active']), 10)}`;
+            statesMessage += `| ${addWhiteSpace(toIndianFormat(state['recovered']), 10)}`;
+            statesMessage += `| ${addWhiteSpace(toIndianFormat(state['deaths']), 8)}`;
         }
 
-        description += '```';
+        statesMessage += '```';
 
-        const casesEmbed = new Discord.MessageEmbed()
-            .setTitle(`COVID-19 Cases by State`)
-            .setDescription(description);
-
-        await message.channel.send(casesEmbed);
+        let splitOptions = {
+            prepend : '```',
+            append : '```'
+        }
+        await message.channel.send(statesMessage, {split: splitOptions});
     },
 };
 
