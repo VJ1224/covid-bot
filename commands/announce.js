3113b4ecea7e49b4346381519752e76e8d4053f0
VJ1224 <vanshjain1224@gmail.com>
2020-08-27 18:46:39 +0530

    Testing webhooks

---
+++ commands/announce.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'announce',
+    description: 'Announce COVID-19 India Bot updates.',
+    usage: ' ',
+    execute: async function (message, args) {
+        if (message.author.id !== '235414130197004288') return;
+        const webhookClient = new Discord.WebhookClient(process.env.WEBHOOK_ID, process.env.WEBHOOK_TOKEN);
+        const text = args.join(' ')
+        await webhookClient.send(text);
+    },
+};
