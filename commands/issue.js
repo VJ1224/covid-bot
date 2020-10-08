7aa102738c1a42d9efaa80f18526b5887d816759
VJ1224 <vanshjain1224@gmail.com>
2020-10-08 14:11:49 +0530

    Added issue command

---
+++ commands/issue.js
@@ -0,0 +1,16 @@
+const { MessageEmbed } = require('discord.js');
+require('dotenv').config();
+
+module.exports = {
+    name: 'issue',
+    description: 'Report issues to the bot owner',
+    usage: ' ',
+    execute: async function (message) {
+        const messageEmbed = new MessageEmbed()
+            .setTitle('Report Issue')
+            .addField('GitHub Repo', '[COVID-19 India Bot Issues](https://github.com/VJ1224/covid-bot/issues)')
+            .addField('Official Server', '[Join Vansh Jain\'s Server](https://discord.gg/4yBuYqK)');
+
+        await message.author.send(messageEmbed);
+    },
+};
